d1c8df48cf7a8b8b8fe22ab746fab8aa3085d937
judoittt <your_email@example.com>
2026-04-28 01:20:30 +0700

    Da doone

--- server/check_db.php
+++
@@ -1,4 +0,0 @@
-<?php
-$c = new mysqli('localhost', 'root', '', 'tracnghiem', 3307);
-$r = $c->query('SHOW COLUMNS FROM api_logs');
-while($row = $r->fetch_assoc()) echo $row['Field'] . " | ";
--- server/test_logs.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once __DIR__ . "/model/admin/logs.model.php";
-$logs = get_api_logs([]);
-echo json_encode(["data" => $logs]);
